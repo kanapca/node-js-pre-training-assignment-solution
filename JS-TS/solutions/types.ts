44f59587994b335804e1c07d714c1efc6fe076ff
Kirill <zuzakirill3@gmail.com>
2026-07-19 14:28:46 +0300

    Created interface Todo w/ required properties

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,15 @@
 interface Todo {
-
+    id: number;
+    title: string;
+    description: string;
+    status: TodoStatus;
+    createdAt: Date;
 }
 
 enum TodoStatus {
-
+    
 }
+
+
 
 export { Todo, TodoStatus };
