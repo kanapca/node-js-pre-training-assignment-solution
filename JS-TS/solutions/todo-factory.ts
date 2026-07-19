939fb5a6d655938e9dba6818c08c3c1466fc289f
Kirill <zuzakirill3@gmail.com>
2026-07-19 21:43:44 +0300

    Implemented createTodo in todo-factory.ts

--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -3,5 +3,14 @@ import { Todo, NewTodo, TodoStatus } from './types';
 let nextId = 1;
 
 export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+  const todo: Todo = {
+    title: input.title,
+    description: input.description,
+    status: TodoStatus.PENDING,
+    id: nextId,
+    createdAt: new Date()
+  }
+
+  nextId++;
+  return todo;
 }
