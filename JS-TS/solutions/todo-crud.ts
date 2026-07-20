2bafc69bc3e38900ca367c4768ebb0794e1ebe19
Kirill <zuzakirill3@gmail.com>
2026-07-20 11:47:48 +0300

    Implementted updateTodo() in todo-crud.ts

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -7,10 +7,29 @@ export function addTodo(state: Todo[], todo: Todo): Todo[] {
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+  if(state == null || state == undefined) {
+    throw new Error("Can't update todo that wasn't added/doesn't exist");
+  }
+  const result: Todo[] = [...state];
+  for(let i in result) {
+    if(result[i].id == id) {
+      result[i] = {...result[i], ...update};
+      return result;
+    }
+  }
+  
+  throw new Error(`Todo with id ${id} not found`);
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
+  if(state == null || state == undefined) {
+    throw new Error("Can't remove todo that wasn't added/doesn't exist");
+  }
+  const result: Todo[] = state;
+  for(let i in result) {
+    if(result[i].id = id) {
+    }
+  }
   throw new Error('removeTodo: not implemented');
 }
 
