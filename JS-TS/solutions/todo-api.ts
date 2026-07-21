c5924c2e802cbba3439d0a7536db6d7120888bbd
Kirill <zuzakirill3@gmail.com>
2026-07-21 22:57:19 +0300

    added error handling in todo-api.ts

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -2,13 +2,29 @@ import { InMemoryRepository } from './repository';
 import { Todo, NewTodo } from './types';
 import { createTodo } from './todo-factory';
 import { updateTodo, removeTodo } from './todo-crud';
-import { filterArray } from './array-helpers';
+
+class TodoNotFoundError extends Error {
+  constructor(id: number) {
+    if(id < 0 || id === 0) super("Can't have a Todo with negative id");
+    super(`Todo with id ${id} was not found`);
+    this.name = "TodoNotFoundError";
+  }
+}
 
 export class TodoApi {
   private repo = new InMemoryRepository<Todo>();
   private item: Todo[] = [];
   private random = Math.random() * 300;
 
+  isTodoFound(id: number) {
+    let index = -1;
+    for(let i in this.item) {
+      if(this.item[i].id == id) index = id;
+    }
+    if(index === -1) {
+      throw new TodoNotFoundError(id);
+    }
+  }
 
   async getAll(): Promise<Todo[]> {
     return new Promise(resolve => {
@@ -23,27 +39,40 @@ export class TodoApi {
     return new Promise(resolve => {
       setTimeout(() => {
         this.item.push(createTodo(newTodo));
-        resolve(createTodo(newTodo))
+        resolve(this.item[this.item.length])
       }, this.random)
     });
     //throw new Error('add: not implemented');
   }
 
   async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        const updatedToDo = updateTodo(this.item, id, update);
-        resolve(updatedToDo[0]);
+        try {
+          this.isTodoFound(id);
+          const updatedToDo = updateTodo(this.item, id, update);
+          resolve(updatedToDo[0]);
+        } catch(error) {
+          reject(error);
+        }
+        
+        
       }, this.random)
     })
     //throw new Error('update: not implemented');
   }
 
   async remove(id: number): Promise<void> {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        this.item = removeTodo(this.item, id);
-        resolve();
+        try{
+          this.isTodoFound(id);
+          this.item = removeTodo(this.item, id);
+          resolve();
+        } catch(error) {
+          reject(error);
+        }
+        
       }, this.random)
     })
     //throw new Error('remove: not implemented');
