586df167e72f79c333b9d7bbeee1b42171876959
Kirill <zuzakirill3@gmail.com>
2026-07-21 19:56:20 +0300

    Implemented getAll() and add() to todo-api.ts, MUST refactor later

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,15 +1,30 @@
 import { InMemoryRepository } from './repository';
 import { Todo, NewTodo } from './types';
+import { createTodo } from './todo-factory';
 
 export class TodoApi {
   private repo = new InMemoryRepository<Todo>();
+  private item: Todo[] = [];
+  private random = Math.random() * 300;
 
   async getAll(): Promise<Todo[]> {
-    throw new Error('getAll: not implemented');
+    let random = Math.random() * 300;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([...this.item]);
+      }, this.random)
+    })
+    //throw new Error('getAll: not implemented');
   }
 
   async add(newTodo: NewTodo): Promise<Todo> {
-    throw new Error('add: not implemented');
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.item.push(createTodo(newTodo));
+        resolve(createTodo(newTodo))
+      }, this.random)
+    })
+    //throw new Error('add: not implemented');
   }
 
   async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
@@ -19,4 +34,6 @@ export class TodoApi {
   async remove(id: number): Promise<void> {
     throw new Error('remove: not implemented');
   }
+
+  
 }
