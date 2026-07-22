9d642c7c719402a5016935bc2b3add0fbff7d9c8
Kirill <zuzakirill3@gmail.com>
2026-07-22 11:22:54 +0300

    Implemented create() in todo-service.ts

--- JS-TS/solutions/todo-service.ts
+++ JS-TS/solutions/todo-service.ts
@@ -1,11 +1,12 @@
 import { TodoApi } from './todo-api';
-import { Todo } from './types';
+import { Todo, TodoStatus } from './types';
 
 export class TodoService {
   constructor(private readonly api: TodoApi) { }
 
   async create(title: string, description = ''): Promise<Todo> {
-    throw new Error('create: not implemented');
+    return this.api.add({title, description})
+    //throw new Error('create: not implemented');
   }
 
   async toggleStatus(id: number): Promise<Todo> {
