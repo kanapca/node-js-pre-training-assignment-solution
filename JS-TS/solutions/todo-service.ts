de329920483a459e4ba56e7e389c0f748516e42c
Kirill <zuzakirill3@gmail.com>
2026-07-22 12:00:06 +0300

    Implemented toggleStatus() in todo-service.ts

--- JS-TS/solutions/todo-service.ts
+++ JS-TS/solutions/todo-service.ts
@@ -7,12 +7,20 @@ export class TodoService {
 
   async create(title: string, description = ''): Promise<Todo> {
     return this.api.add({title, description})
-    //throw new Error('create: not implemented');
   }
 
   async toggleStatus(id: number): Promise<Todo> {
+    const todos = await this.api.getAll();
+    const result = filterArray(todos, todo => todo.id === id);
 
-    throw new Error('toggleStatus: not implemented');
+    let newStatus: TodoStatus;
+    switch(result[0].status) {
+      case TodoStatus.COMPLETED: newStatus = TodoStatus.IN_PROGRESS; break;
+      case TodoStatus.IN_PROGRESS: newStatus = TodoStatus.COMPLETED; break;
+      case TodoStatus.PENDING: newStatus = TodoStatus.IN_PROGRESS; break;
+      default: newStatus = TodoStatus.PENDING; break;
+    }
+    return this.api.update(id, {status: newStatus});
   }
 
   async search(keyword: string): Promise<Todo[]> {
