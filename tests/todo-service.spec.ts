a55611529111596020f29b4bcb679097d52c4649
Kirill <zuzakirill3@gmail.com>
2026-07-24 21:15:40 +0300

    Created basic tests for todo-service

---
+++ tests/todo-service.spec.ts
@@ -0,0 +1,24 @@
+import { TodoApi } from '../JS-TS/solutions/todo-api';
+import { TodoService } from "../JS-TS/solutions/todo-service";
+import { InMemoryRepository } from "../JS-TS/solutions/repository";
+
+describe('create', () => {
+  jest.setTimeout(1000);
+  const service = new TodoService(new TodoApi());
+
+  it('create() adds todo', async() => {
+      const created = await service.create('Service Item');
+      expect(created.title).toBe('Service Item');
+  })
+
+  it('toggleStatus() should change status', async() => {
+    const [todo] = await service.search('service');
+    const toggled = await service.toggleStatus(todo.id);
+    expect(toggled.status).not.toBe(todo.status);
+  })
+
+  it('search() shpuld return matching items', async() => {
+    const list = await service.search('SERVICE');
+    expect(list.length).toBeGreaterThan(0);
+  })
+})
