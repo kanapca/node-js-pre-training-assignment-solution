bff243f72d4c19972f562802f60614ba10e3ec36
Kirill <zuzakirill3@gmail.com>
2026-08-09 22:13:07 +0300

    implemented remove for todo api

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,7 +1,7 @@
 import { filterArray } from "./array-helpers";
 import { Todo, NewTodo } from "./types";
 import { createTodo } from "./todo-factory";
-import { addTodo, updateTodo } from "./todo-crud";
+import { addTodo, updateTodo, removeTodo } from "./todo-crud";
 
 class TodoNotFoundError extends Error {
    constructor(id: number) {
@@ -15,34 +15,50 @@ class TodoNotFoundError extends Error {
 }
 
 export class TodoApi {
-    private random = Math.random() * 300;
     private api: Todo[] = [];
 
     async getAll(): Promise<Todo[]> {
+        let random = Math.random() * 300;
+
         return new Promise(resolve => {
             setTimeout(() => {
                 resolve([...this.api]);
-            }, this.random);
+            }, random);
         });
     }
 
     async add(newTodo: NewTodo): Promise<Todo> {
+        let random = Math.random() * 300;
+
         return new Promise(resolve => {
             setTimeout(() => {
                 const todo = createTodo(newTodo)
                 this.api = [...this.api, todo];
                 resolve({...todo});
-            }, this.random);
+            }, random);
         });
     }
 
     async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
+        let random = Math.random() * 300;
+
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 this.api = updateTodo(this.api, id, update);
                 const updated = filterArray(this.api, todo => todo.id === id);
                 resolve(updated[0]!);
-            }, this.random);
+            }, random);
         });
+    }
+
+    async remove(id: number): Promise<void> {
+        let random = Math.random() * 300;
+        
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                this.api = removeTodo(this.api, id);
+                resolve();
+            }, random);
+        })
     }
 }
