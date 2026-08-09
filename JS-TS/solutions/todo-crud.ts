41fd0581406ad19c9baad0c9a69f2b59bb22008d
Kirill <zuzakirill3@gmail.com>
2026-08-09 18:35:42 +0300

    Implemented removeTodo()

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -2,21 +2,35 @@ import { Todo } from "./types";
 import { createTodo } from "./todo-factory";
 import { filterArray, mapArray } from "./array-helpers";
 
+function idSearch(state: Todo[], id: number) {
+    let isFound = false;
+    for(let item of state) {
+        if(item.id === id) isFound = true;
+    }
+    return isFound;
+}
+
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
     return [...state, todo]
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
+    if(!idSearch(state, id)) {
+        throw new Error(`Can't update todo with id ${id}`);
+    }
     let result: Todo[] = state;
-    
-    filterArray(result, todo => todo.id === id);
-
     result = mapArray(result, todo => todo.id === id ? {...todo, ...update} as Todo : todo);
     return result;
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
+    if(!idSearch(state, id)) {
+        throw new Error(`Can't remove todo with id ${id}`);
+    }
+    let result: Todo[] = state;
+    result = filterArray(result, todo => todo.id != id);
 
+    return result;
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
