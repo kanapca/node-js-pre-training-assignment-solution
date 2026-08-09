d312ee6feb2cb09bc8f2b40070f859a5c180c758
Kirill <zuzakirill3@gmail.com>
2026-08-09 22:06:05 +0300

    added update function for api

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,6 +1,7 @@
 import { filterArray } from "./array-helpers";
 import { Todo, NewTodo } from "./types";
 import { createTodo } from "./todo-factory";
+import { addTodo, updateTodo } from "./todo-crud";
 
 class TodoNotFoundError extends Error {
    constructor(id: number) {
@@ -31,6 +32,16 @@ export class TodoApi {
                 const todo = createTodo(newTodo)
                 this.api = [...this.api, todo];
                 resolve({...todo});
+            }, this.random);
+        });
+    }
+
+    async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                this.api = updateTodo(this.api, id, update);
+                const updated = filterArray(this.api, todo => todo.id === id);
+                resolve(updated[0]!);
             }, this.random);
         });
     }
