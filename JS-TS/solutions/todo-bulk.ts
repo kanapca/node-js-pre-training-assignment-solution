f1edc6c89235add4c8583944300e1c3a51ef94c6
Kirill <zuzakirill3@gmail.com>
2026-08-09 21:11:41 +0300

    Implemented toggleAll() function

--- JS-TS/solutions/todo-bulk.ts
+++ JS-TS/solutions/todo-bulk.ts
@@ -1,23 +1,10 @@
-import { Todo, TodoStatus } from './types';
-import { mapArray, filterArray, reduceArray } from './array-helpers';
+import { mapArray } from "./array-helpers";
+import { Todo, TodoStatus } from "./types";
+import { updateTodo } from "./todo-crud";
 
 export function toggleAll(state: Todo[], completed: boolean): Todo[] {
-  const newStatus = completed ? TodoStatus.COMPLETED : TodoStatus.PENDING;
-  return mapArray(state, (todo) => ({...todo, status: newStatus}))
-  //throw new Error('toggleAll: not implemented');
-}
-
-export function clearCompleted(state: Todo[]): Todo[] {
-  return filterArray(state, todo => todo.status === TodoStatus.PENDING || todo.status === TodoStatus.IN_PROGRESS);
-  //throw new Error('clearCompleted: not implemented');
-}
-
-export function countByStatus(state: Todo[], status: TodoStatus): number {
-  return reduceArray(state, (count, todo) => {
-    if(todo.status === status) {
-      count++;
-    }
-    return count;
-  }, 0)
-  //throw new Error('countByStatus: not implemented');
+    return mapArray(state, todo => ({ 
+        ...todo, 
+        status: completed ? TodoStatus.COMPLETED: TodoStatus.PENDING
+    }));
 }
