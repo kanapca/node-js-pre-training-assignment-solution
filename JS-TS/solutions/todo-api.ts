f2edd1393a03a0a9700280b0526465768ed42a49
Kirill <zuzakirill3@gmail.com>
2026-08-09 21:48:13 +0300

    Implemented add and getAll for api

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,82 +1,37 @@
-import { InMemoryRepository } from './repository';
-import { Todo, NewTodo } from './types';
-import { createTodo } from './todo-factory';
-export class TodoNotFoundError extends Error {
-  constructor(id: number) {
-    if(id < 0) {
-      super("Can't have a Todo with negative id");
+import { filterArray } from "./array-helpers";
+import { Todo, NewTodo } from "./types";
+import { createTodo } from "./todo-factory";
+
+class TodoNotFoundError extends Error {
+   constructor(id: number) {
+    if(id < 0) { 
+        super("Can't have a todo with negative id");
     } else {
-      super(`Todo with id ${id} was not found`);
+        super(`Todo with id ${id} was not found`);
     }
     this.name = "TodoNotFoundError";
-  }
+   }
 }
 
 export class TodoApi {
-  private repo = new InMemoryRepository<Todo>();
-  private random = Math.random() * 300;
+    private random = Math.random() * 300;
+    private api: Todo[] = [];
 
-  isTodoFound(id: number) {
-    let index = -1;
-    let arr = this.repo.findAll();
-    if(!arr) {
-      throw new Error("No todos");
+    async getAll(): Promise<Todo[]> {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve([...this.api]);
+            }, this.random);
+        });
     }
-    for(let i in arr) {
-      if(arr[i]!.id == id) index = id;
+
+    async add(newTodo: NewTodo): Promise<Todo> {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                const todo = createTodo(newTodo)
+                this.api = [...this.api, todo];
+                resolve({...todo});
+            }, this.random);
+        });
     }
-    if(index === -1) {
-      throw new TodoNotFoundError(id);
-    }
-  }
-
-  async getAll(): Promise<Todo[]> {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(this.repo.findAll());
-      }, this.random)
-    });
-    //throw new Error('getAll: not implemented');
-  }
-
-  async add(newTodo: NewTodo): Promise<Todo> {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(this.repo.add(createTodo(newTodo)))
-      }, this.random)
-    });
-    //throw new Error('add: not implemented');
-  }
-
-  async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        try {
-          this.isTodoFound(id);
-          resolve(this.repo.update(id, update));
-        } catch(error) {
-          reject(error);
-        }
-        
-        
-      }, this.random)
-    })
-    //throw new Error('update: not implemented');
-  }
-
-  async remove(id: number): Promise<void> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        try{
-          this.isTodoFound(id);
-          resolve(this.repo.remove(id));
-        } catch(error) {
-          reject(error);
-        }
-        
-      }, this.random)
-    })
-    //throw new Error('remove: not implemented');
-  }
-  
 }
