505595ea267ec51914bfff411b113e285bcfe830
Kirill <zuzakirill3@gmail.com>
2026-08-10 17:07:14 +0300

    Implemented add handling

--- JS-TS/solutions/index.ts
+++ JS-TS/solutions/index.ts
@@ -1,3 +1,39 @@
-#!/usr/bin/env ts-node
-// CLI entry for Task 10 – placeholder only
-console.log('CLI not implemented yet');
+import { Todo } from "./types";
+import { ToDoManager } from "./todo-manager";
+
+class CLI {
+    private manager = new ToDoManager();
+
+    async run(): Promise<void> {
+        const args = process.argv.slice(2);
+        const command = args[0];
+
+        if(!command) {
+            return;
+        }
+
+        try {
+            await this.manager.init();
+
+            switch(command) {
+                case "add": await this.handleAdd(args); break;
+            }
+        } catch(error) {
+            console.error("Error");
+        }
+    }
+
+    
+    private async handleAdd(args: string[]): Promise<void> {
+        const title = args[1];
+        const description = args[2] || undefined;
+
+        if(!title) {
+            console.error("Can't create a todo without a title");
+            return;
+        }
+
+        await this.manager.add(title, description);
+        console.log(`Added todo "${title}"`);
+    }
+}
