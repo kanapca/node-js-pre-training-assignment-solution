662aea6144ef71383265a267096f8ef7d63bd661
Kirill <zuzakirill3@gmail.com>
2026-09-05 11:06:41 +0300

    Implemented logger, timer, header injector and sequence tracker

--- Node-Express-Nest/v1/solutions/express/task-01.ts
+++ Node-Express-Nest/v1/solutions/express/task-01.ts
@@ -3,11 +3,36 @@ import express, { type Express, type Request, type Response, type NextFunction }
 const APP: Express = express();
 const PORT = 3000;
 
-APP.use((req: Request, res: Response, next: NextFunction) => {
-    const start = Date.now();
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} - ${Date.now() - start}ms`);
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[LOGGER] ${req.method} ${req.url}`);
     next();
-})
+}
+
+const timer = (req: Request, res: Response, next: NextFunction) => {
+    const start = Date.now();
+    res.on('finish', () => {
+        console.log(`[TIMER] ${req.method} ${req.url} - ${Date.now() - start}ms`);
+    });
+    next();
+};
+
+const headerInjector = (req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Custom', 'my-express-app');
+    res.setHeader('Request-id', crypto.randomUUID());
+    res.setHeader('App-Version', '1.0.0');
+    next();
+};
+
+const sequenceTracker = (label: string) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        console.log(`[Sequence] -> ${label}`);
+        next();
+    }
+}
+
+APP.use(sequenceTracker('logger (start)'), logger);
+APP.use(sequenceTracker('timer (start)'), timer);
+APP.use(sequenceTracker('header (start)'), headerInjector);
 
 APP.get('/', (req: Request, res: Response) => {
     res.send('hello, world!');
